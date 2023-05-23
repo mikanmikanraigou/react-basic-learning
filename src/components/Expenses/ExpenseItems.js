bb2538460350666da31072ca0de2bf6f6558163c
Galang I Q <galang.izqd@gmail.com>
2023-05-23 14:23:25 +0700

    refactor directory

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,8 +1,8 @@
 import "./ExpenseItem.css";
-import "./Card.css";
+import "../UI/Card.css";
 
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 
 const ExpenseItem = (prop) => {
